cde3e72aeeb4ecff0f68527092cda1d72794405e
Tyckong <114450886+Tyckong@users.noreply.github.com>
2024-09-25 16:47:41 -0300

    Update 3 main.cpp
    Jaccard is now declared outside the main function and main function implements it.

--- main.cpp
+++ main.cpp
@@ -3,51 +3,16 @@
 #include <iostream>
 #include <fstream>
 #include "hyperloglog.cpp"
+#include "jaccard.cpp"
 
 int main() {
 
 	unsigned int seed = rand();
 	map<int, int> GenA = hyperloglog("GCF_000331305.1_ASM33130v1_genomic.fna", seed);
 	map<int, int> GenB = hyperloglog("GCF_000959725.1_ASM95972v1_genomic.fna", seed);
-	map<int, int> GenAoB;
 
-	int cardA = 0;
-	int cardB = 0;
-	int cardAoB = 0;
-	for(auto p: GenA)
-		cardA += p.second;
+	float jaccard = jaccard_function(GenA, GenB);
 
-	for(auto p: GenB)
-		cardB += p.second;
-
-	map<int, int>::iterator itA = GenA.begin();
-	map<int, int>::iterator itB = GenB.begin();
-
-	if(cardA<cardB){
-		while(itA != GenA.end()){
-			if(itA->first == itB->first){
-				GenAoB.insert(pair<int, int>(itA->first, itA->second));
-			}
-			advance(itA, 1);
-			advance(itB, 1);
-		}
-	}else{
-		while(itB != GenB.end()){
-			if(itA->first == itB->first){
-				GenAoB.insert(pair<int, int>(itA->first, itA->second));
-			}
-			advance(itA, 1);
-			advance(itB, 1);
-		}
-	}
-	for(auto p: GenAoB)
-		cardAoB += p.second;
-
-	cout << "Cardinalidad A: " << cardA << endl;
-	cout << "Cardinalidad B: " << cardB << endl;
-	cout << "Cardinalidad AoB: " << cardAoB << endl;
-
-	float Jaccard = (cardA + cardB - cardAoB) / cardAoB; 
-	cout << "Jaccard: " << Jaccard << endl;
+	cout << "Jaccard: " << jaccard << endl;
 	return 0;
 }
