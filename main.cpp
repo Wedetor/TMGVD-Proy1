e1118b2dff84f4499568b2d744aad37bd8183ede
Wedetor <joscortes2021@udec.cl>
2024-09-25 19:08:39 -0300

    Added new genome files. Updated main.cpp with their names.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "hyperloglog.cpp"
 #include "jaccard.cpp"
 #include "realJaccard.cpp"
@@ -9,13 +10,24 @@
 int main() {
 
 	unsigned int seed = rand();
-	map<unsigned int, unsigned int> GenA = hyperloglog("GCF_000331305.1_ASM33130v1_genomic.fna", seed);
-	map<unsigned int, unsigned int> GenB = hyperloglog("GCF_000959725.1_ASM95972v1_genomic.fna", seed);
+
+	string GenAName = "GCF_000331305.1_ASM33130v1_genomic.fna";
+	string GenBName = "GCF_000959725.1_ASM95972v1_genomic.fna";
+	string GenCName = "GCF_000373685.1_ASM37368v1_genomic.fna";
+	string GenDName = "GCF_000583735.1_ASM58373v1_genomic.fna";
+	string GenEName = "GCF_001189295.1_ASM118929v1_genomic.fna";
+
+	map<unsigned int, unsigned int> GenA = hyperloglog(GenAName, seed);
+	map<unsigned int, unsigned int> GenB = hyperloglog(GenBName, seed);
+	map<unsigned int, unsigned int> GenC = hyperloglog(GenCName, seed);
+	map<unsigned int, unsigned int> GenD = hyperloglog(GenDName, seed);
+	map<unsigned int, unsigned int> GenE = hyperloglog(GenEName, seed);
 
 	float jaccard = jaccard_function(GenA, GenB);
-	float rejaccard = realJaccard("GCF_000331305.1_ASM33130v1_genomic.fna", "GCF_000959725.1_ASM95972v1_genomic.fna");
+	float rejaccard = realJaccard(GenAName, GenBName);
 
-	cout << "Jaccard: " << fixed << jaccard << endl;
+	cout << "Jaccard of GenA & GenB: " << fixed << jaccard << endl;
 	cout << "Real Jaccard: " << fixed << rejaccard << endl;
+
 	return 0;
 }
