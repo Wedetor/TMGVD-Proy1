c6e33537cabb305c20ec8ae883b48434f1874a4e
Tyckong <114450886+Tyckong@users.noreply.github.com>
2024-09-25 15:55:13 -0300

    Update main.cpp
    Coded the first model for the Jaccard implementation.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,44 @@
 int main() {
 
 	unsigned int seed = rand();
-	hyperloglog("GCF_000331305.1_ASM33130v1_genomic.fna", seed);
+	map<int, int> GenA = hyperloglog("GCF_000331305.1_ASM33130v1_genomic.fna", seed);
+	map<int, int> GenB = hyperloglog("GCF_000959725.1_ASM95972v1_genomic.fna", seed);
+	map<int, int> GenAoB;
+
+	int cardA = 0;
+	int cardB = 0;
+	int cardAoB = 0;
+	for(auto p: GenA)
+		cardA += p.second;
 	
+	for(auto p: GenB)
+		cardB += p.second;
+
+	map<int, int>::iterator itA = GenA.begin();
+	map<int, int>::iterator itB = GenB.begin();
+
+	if(cardA<cardB){
+		while(itA != GenA.end()){
+			if(itA->first == itB->first){
+				GenAoB.insert(pair<int, int>(itA->first, itA->second));
+			}
+		}
+	}else{
+		while(itB != GenB.end()){
+			if(itA->first == itB->first){
+				GenAoB.insert(pair<int, int>(itA->first, itA->second));
+			}
+		}
+	}
+
+	for(auto p: GenAoB)
+		cardAoB += p.second;
+
+	cout << "Cardinalidad A: " << cardA << endl;
+	cout << "Cardinalidad B: " << cardB << endl;
+	cout << "Cardinalidad AoB: " << cardAoB << endl;
+
+	int Jaccard = (cardA + cardB - cardAoB) / cardAoB; 
+	cout << "Jaccard: " << Jaccard << endl;
 	return 0;
 }
